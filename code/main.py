540beaf538a044364e4f09903f40c459ec472291
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2022-04-17 13:55:51 -0300

    adição da escrita no arquivo

--- code/main.py
+++ code/main.py
@@ -1,6 +1,17 @@
 import get_summoner_info as gsi
 
 
+def write_file(
+    path: str,
+    text: str
+):
+    with open(
+        file=path,
+        mode="w"
+    ) as f:
+        f.write(text)
+
+
 def main(
     configs: dict
 ):
@@ -9,13 +20,19 @@ def main(
     )
 
     pdl = summoner_info["leaguePoints"]
+    tier = summoner_info["tier"]
 
-    print(pdl)
+    text = f"{str(tier).title()} {pdl} pdl"
+
+    write_file(
+        path="./pdl.txt",
+        text=text
+    )
 
 
 if __name__ == "__main__":
     import tools as t
-    
+
     configs = t.get_yaml_configs()
 
     main(
