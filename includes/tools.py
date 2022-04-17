609ea8a7eb88f95f18fc07f2c103bf05ca10845f
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2022-04-17 12:43:21 -0300

    add type file

--- includes/tools.py
+++ includes/tools.py
@@ -1,7 +1,9 @@
 import yaml
 
 
-def get_yaml_configs(file="./config.yaml"):
+def get_yaml_configs(
+    file: str = "./config.yaml"
+):
     with open(file, "r") as y:
         yc = yaml.load(y, Loader=yaml.Loader)
 
