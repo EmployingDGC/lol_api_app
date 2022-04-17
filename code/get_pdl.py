56c1279cf7de0d2eb89741cc0e79dbb3cecad1f8
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2022-04-17 12:33:16 -0300

    resolvendo import

--- code/get_pdl.py
+++ code/get_pdl.py
@@ -1,5 +1,16 @@
+if __name__ == "__main__":
+    import sys
+    import os
+
+    SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+
+import includes.tools as t
+
+
 def main():
-    print("Hello world!")
+    print(t.get_yaml_configs())
 
 
 if __name__ == "__main__":
